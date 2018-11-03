2f9ffbf1be2e1d3b027274cb022e40267bfe77af
fixikee <renato.kazinczi@gmail.com>
2018-11-03 20:36:09 +0100

    recipe list ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'test', 'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg'),
+    new Recipe('A test recipe', 'test', 'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg')
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,11 @@
 export class Recipe {
   public name: string;
-  public descreption: string;
+  public description: string;
   public imagePath: string;
 
   constructor(name: string, desc: string, imagePath: string) {
     this.name = name;
-    this.descreption = desc;
+    this.description = desc;
     this.imagePath = imagePath;
   }
 }
